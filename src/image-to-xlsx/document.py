1974c02945f3138adf36f7434c480d93522d1a72
lbm364dl <geometrydashm364@gmail.com>
2024-11-26 16:39:40 +0100

    Format

--- src/image-to-xlsx/document.py
+++ src/image-to-xlsx/document.py
@@ -1,6 +1,7 @@
 from surya.input.load import load_from_file
 from surya.settings import settings
 
+
 class Document:
     def __init__(self, path):
         self.pages, _, _ = load_from_file(path, dpi=settings.IMAGE_DPI_HIGHRES)
